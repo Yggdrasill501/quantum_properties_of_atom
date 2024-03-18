ee3773ae9459715843f7a395d08b707a416b8e38
Filip Žitný <filipzitny@gmail.com>
2024-03-18 15:35:09 +0100

    photon voltage

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,29 @@
 # First experiment
 
-# Errors
+# Errors 
 OFFSET_MULTIMETER_1 = float(003)
 OFFSET_MULTIMETER_2 = float(002)
 
+photon_volatage={
+    "shade1": {
+        578: 360,
+        546: 514,
+        436: 825,
+        405: 715,
+        365: 1040
+        },
+    "shade2": {
+        578: 665,
+        546: 775,
+        436: 1155,
+        405: 1280,
+        365: 1620
+        },
+    "shade3": {
+        578: 550,
+        546: 695,
+        436: 1174,
+        405: 1310,
+        365: 1650
+        }
+}
